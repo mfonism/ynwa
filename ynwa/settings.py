bb8612c11952ec89b61690ca6e1f0660069929c2
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-24 15:17:39 +0100

    clean up settings, again

--- ynwa/settings.py
+++ ynwa/settings.py
@@ -125,7 +125,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # TINYMCE
