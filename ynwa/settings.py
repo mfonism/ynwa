7425f06a0848ab343c8063b34afdcb23fc6a88a0
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-24 12:01:14 +0100

    add styles for tinymce-lite editor

--- ynwa/settings.py
+++ ynwa/settings.py
@@ -131,7 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # TINYMCE
 TINYMCE_DEFAULT_CONFIG = {
     "height": 360,
-    "width": 360,
     "cleanup_on_startup": True,
     "custom_undo_redo_levels": 20,
     "selector": "textarea",
