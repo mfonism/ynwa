2a28a050854a0536642955623ea86e1c297f67b5
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-25 02:17:27 +0100

    enable code syntax highlighting
    Add codesample plugin
    Add preview plugin
    Specify the necessary CSS and JS files in the templates

--- ynwa/settings.py
+++ ynwa/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # TINYMCE
 
