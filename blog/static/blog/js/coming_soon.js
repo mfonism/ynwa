b523025a5ef2d086ab21be5d14dd5a1d9eba73d8
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-01-01 07:16:07 +0100

    correct error with launch date
    Months are indexed from 0 in JS Date objects.
    Change launch month to 1 (February) from 2 (March).

--- blog/static/blog/js/coming_soon.js
+++ blog/static/blog/js/coming_soon.js
@@ -4,9 +4,9 @@ $(function () {
     'use strict'
 
     $(document).ready(function() {
-        var date = new Date(2020, 2, 29);
+        var launch_date = new Date(2020, 1, 29);
         var now = new Date();
-        var diff = (date.getTime()/1000) - (now.getTime()/1000);
+        var diff = (launch_date.getTime()/1000) - (now.getTime()/1000);
 
         var clock = $('#flipClock').FlipClock(diff, {
             clockFace: "DailyCounter",
